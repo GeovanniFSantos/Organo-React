62fff3eba5bf29c4c419a7b06e88c43f6ed35dc6
Geovanni Santos <117683781+Geovannydynoo@users.noreply.github.com>
2023-08-30 21:21:41 -0300

    Primeiro comentario

---
+++ src/componentes/CampoText/index.js
@@ -0,0 +1,23 @@
+import './CampoText.css'
+
+const CampoText = (props) => {
+
+    const labelModificada = `${props.label}`
+    const placeholderModificado = `${props.placeholder}`
+
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className='campo-text'>
+            <label>{labelModificada}</label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificado} />
+        </div>
+
+    )
+
+}
+
+export default CampoText 
